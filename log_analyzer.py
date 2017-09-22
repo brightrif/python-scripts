502eec013d8c78a0b13f3dc1e28ca968f75db245
Bright Antony <brightantony.g@gmail.com>
2017-09-22 20:38:25 +0300

    Create log_analyzer.py

---
+++ log_analyzer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import MySQLdb
+import csv
+
+#connect
+db = MySQLdb.connect(host="localhost",user="admin",passwd="indra2012",db="ihs_logs")
+
+cursor = db.cursor()
+
+with open ('test.log') as readlog:
+        cr = csv.reader(readlog, delimiter='\t')
+
+
+        for row in cr:
+
+
+# Execute SQL select statement
+                qry = 'insert into logs (ip,date,time,method,url,referance,status,bytes,timetaken) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+                cursor.execute(qry,row)
+                db.commit()
+
+
+# Close the connection
+        db.close()
